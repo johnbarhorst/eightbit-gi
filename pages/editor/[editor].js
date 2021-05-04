0dc7b121dac543a1f7d4aa08ba97e9f281c0ccd5
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-05-04 00:11:23 -0500

    loading fall back to fix prerender bug

--- pages/editor/[editor].js
+++ pages/editor/[editor].js
@@ -3,6 +3,7 @@ import { EditorPage } from 'components/EditorPage';
 
 export default function Editor() {
   const router = useRouter();
-  const { editor } = router.query;
+  const { editor } = router?.query;
+  if(router.isFallback) return <div>Loading...</div>;
   return <EditorPage editor={editor} />;
 }
